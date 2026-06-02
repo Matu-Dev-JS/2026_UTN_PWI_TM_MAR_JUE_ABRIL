aa8f52eaa67b7ca326e38331ce45901ef4c74df4
matu.dev <mati.a.gimenez2002@gmail.com>
2026-06-02 12:03:50 -0300

    Clase 15

--- Clase 15 JS/script.js
+++ Clase 15 JS/script.js
@@ -83,4 +83,64 @@ Calcular el promedio de 3 notas.
 5. Mostrar el resultado del promedio al usuario.
 
 NO es necesario usar funciones
+
 */
+
+/* let suma = 0
+const cantidad_notas_promedio = 3
+for(
+    let i = 1;
+    i <= cantidad_notas_promedio;
+    i = i + 1 //Quiero que incremente de 1 en 1
+){
+    let nota = prompt('ingresar nota' ) //string
+    while(nota === null || isNaN(nota) || Number(nota) < 0){
+        nota = prompt('Error al ingresar, seleccione un numero postivo')
+    }
+    suma = suma + Number(nota)
+}
+
+let resultado_promedio = suma / cantidad_notas_promedio
+console.log( 'promedio del usuario ' + resultado_promedio.toFixed(1) ) */
+
+/* 
+EXCEPCION DE COMPARAR NAN:
+NaN == NaN = false
+NaN === NaN = false
+
+Para saber si un dato es NaN debemos usar isNaN, esta funcion nos indica si un dato es o no un NaN NO USAR === NaN o == NaN porque NAN ES INCOMPARABLE
+*/
+
+
+
+//FUNCIONES: 
+/* 
+Poder guardar acciones en memoria
+*/
+
+/* 
+La funcion calcularIva recibe un precio, saca su 21% a lo que llamamos iva y lo muestra por consola
+*/
+function calcularIva (precio){
+    const valor_iva = precio * 0.21 //21%
+    console.log("El valor del iva es $" + valor_iva)
+}
+
+calcularIva(100)
+calcularIva(200)
+calcularIva(1000)
+
+
+//Crear una funcion llamada sumar que recibira 2 parametros (a y b o numero_1 y numero_2) y mostrara por consola 'el resultado de la suma entre {a} y {b} es {resultado}'
+//Si yo invoco sumar(30, 60) me dira por consola 'el resultado de la suma entre 30 y 60 es 90'
+
+function sumar(a, b){
+    a = Number(a)
+    b = Number(b)
+    let resultado = a + b 
+    console.log('el resultado de la suma entre ' + a + ' y ' +  b + ' es ' + resultado)
+}
+
+sumar(30, 20)
+
+
