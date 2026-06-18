be2b2fce42d1967e149f1a824bc399bd72c02557
matu.dev <mati.a.gimenez2002@gmail.com>
2026-06-18 11:32:36 -0300

    Clase 20

--- Clase 20 JS/script.js
+++ Clase 20 JS/script.js
@@ -8,7 +8,7 @@
     ESTADOS:
 */
 
-const estado_tweets = [
+let estado_tweets = [
     {
         id: 1,
         content: 'Cuidado con Cabo Verde!',
@@ -103,8 +103,6 @@ function renderTweets(){
     if (tweets_HTML){
         let resultado = '' 
         for (const tweet of estado_tweets){
-
-            
             resultado = resultado +`
             <div> 
                 <h3>${tweet.author_name}</h3>
@@ -125,12 +123,68 @@ function renderTweets(){
     }
 }
 
-/* Que tipo de dato es url? es un array que contiene strings */
+function findTweetById (tweet_id){
+    for(let tweet of estado_tweets){
+        if(tweet.id === Number(tweet_id)){
+            return tweet
+        }
+    }
+}
+/* function deleteTweetById (tweet_id){
+    const tweet_found = findTweetById(tweet_id)
 
+    //Sino esta devuelvo false indicando que no elimine nada
+    if(!tweet_found){
+        return false
+    }
+
+    const tweet_index = estado_tweets.indexOf(tweet_found)
+
+    console.log('EL indice del tweet a elminar es: ' + tweet_index )
+
+    //Creo un clon para saltear la regla de inmutabilidad de los estados
+    const clone_estado_tweets = [...estado_tweets]
+
+    //Elimina desde el indice de el tweet buscado 1 elemento
+    clone_estado_tweets.splice(tweet_index, 1)
+
+    console.log('Estado clonado', clone_estado_tweets) //Aca deben ser 2
+    console.log('Estado original', estado_tweets) //Aca deben ser 3
+    setEstadoTweets(clone_estado_tweets)
+
+}
+ */
+
+/* function deleteTweetById (tweet_id){
+    const nueva_lista_tweets = []
+
+    for(const tweet of estado_tweets){
+
+        //Dejo pasar a todos los que cumplen con la condicion
+        if(tweet.id !== tweet_id){
+            nueva_lista_tweets.push(tweet)
+        }
+    }
+
+    setEstadoTweets(nueva_lista_tweets)
+} */
+
+/* 
+Crear la funcion agregarTweet que recibira un id, author_name, content y deberan crear un nuevo tweet y agregarlo al estado
+
+A tener en cuenta:
+    - El estado es inmutable
+    - .push() es un metodo mutable
+    - Cuando creen el tweet muchos valores estaran vacion, ejemplo attachments o comments valdran en [] y likes y retweets seran 0
+    - Por ahora la funcion no contempla que puedas crear un tweet con attachments
+*/
 
 
 
 renderTweets()
+
+
+
 
 
 
