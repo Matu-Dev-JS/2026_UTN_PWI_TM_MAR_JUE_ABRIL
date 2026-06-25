e5c7fe5610b896ba34f690d833ebf4c85fece2c1
matu.dev <mati.a.gimenez2002@gmail.com>
2026-06-25 11:57:49 -0300

    Clase 22

--- clase 21 - 22 React/src/App.jsx
+++ clase 21 - 22 React/src/App.jsx
@@ -1,9 +1,39 @@
 import React from 'react'
 
+import { ProductCard } from './Components/ProductCard/ProductCard'
+
+import Titulo from './Components/Titulo/Titulo'
+
+//Las funciones que devuelven HTML se llaman componentes
 function App() {
   return (
-    <div>Hola mundo</div>
+    <div>
+      <Titulo content='Hola a todos!'/>
+      <Titulo content='Que tal??'/>
+      <button>Hola</button>
+
+      <ProductCard 
+        title='tv samsung 50"' 
+        description='lorem, bla bla bla...' 
+        price={400}
+        categories={['Tecnologia', 'mundial']}
+      />
+      <ProductCard 
+        title='tv noblex 50"' 
+        description='lorem, bla bla bla...' 
+        price={300}
+        categories={['Hogar', 'mundial']}
+      />
+      <ProductCard 
+        title='tv LG 50"' 
+        description='lorem, bla bla bla...'
+        price={350}
+        categories={['Tech', 'mundial']}
+      />
+    </div>
   )
 }
+
+
 
 export default App
--- clase 21 - 22 React/src/main.jsx
+++ clase 21 - 22 React/src/main.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+/* import { multiplicar, restar, sumar } from './math.js' */
+//import PI, {restar, multiplicar} from './math.js'
+//import * as Math from './math.js'
+
 
 
 /* LLama al div y renderiza el app dentro de el */
