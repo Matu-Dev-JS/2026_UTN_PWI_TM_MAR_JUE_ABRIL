8482679c1c1d651b40a2e30d5df3969f5e03448f
matu.dev <mati.a.gimenez2002@gmail.com>
2026-06-30 12:00:00 -0300

    Clase 23 estados

--- clase 21 - 22 React/src/App.jsx
+++ clase 21 - 22 React/src/App.jsx
@@ -4,11 +4,28 @@ import Contador from './Components/Contador/Contador'
 
 //Las funciones que devuelven HTML se llaman componentes
 function App() {
+
+  /* 
+    Crear un componente llamado Modal
+    El componente tendra un estado interno llamado openModal
+    El estado sera booleano e indicara si se renderiza o no un
+      <div>
+        <h1>Soy un modal</h1>
+        <button>Cerrar</button>
+      </div>
+    Si el modal esta cerrado debe renderizarse
+      <button>Abrir modal</button>
+    El modal debe funcionar
+  */
   return (
     <div>
 
       <Contador/>
-      
+      {/* <div>
+        <h1>Soy un modal</h1>
+        <button>Cerrar</button>
+      </div> */}
+      <button>Abrir modal</button>
     </div>
   )
 }
