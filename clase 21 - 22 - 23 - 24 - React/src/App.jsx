ff4bb1630287c737bd7cb4d1986c13099da1df72
matu.dev <mati.a.gimenez2002@gmail.com>
2026-07-02 11:53:58 -0300

    Ejemplo mapeo

--- clase 21 - 22 - 23 - 24 - React/src/App.jsx
+++ clase 21 - 22 - 23 - 24 - React/src/App.jsx
@@ -13,11 +13,76 @@ function App() {
     onClose
   } = useModal(true)
 
+
+  const messages = [
+    {
+      id: 1,
+      sendByMe: false,
+      content: "hola!"
+    },
+    {
+      id: 2,
+      sendByMe: true,
+      content: "Todo bien!"
+    },
+    {
+      id: 3,
+      sendByMe: false,
+      content: "Que tal?"
+    },
+    {
+      id: 4,
+      sendByMe: true,
+      content: "Todo bien!"
+    }
+  ]
+
+  const anotaciones = [
+    {
+      id: 1,
+      title: "Cosas que hacer cuando me aburro",
+      content: "Practicar JS... ETC"
+    },
+    {
+      id: 2,
+      title: "Si salimos campeones...",
+      content: "Aprendo PHP"
+    }
+  ]
+
+  /* 
+  Renderizar en pantalla la lista de anotaciones
+  cada anotacion debe verse: 
+    <div>
+      <h2>{title}</h2>
+      <p>{content}</p>
+      <button>Eliminar</button>
+      <button>Editar</button>
+      <button>Ver detalle</button>
+      <hr/>
+    </div>
+  */
   return (
     <div>
+      <h1>Mensajes:</h1>
+      {
+        messages.map(
+          (message) => {
+            return <div>
+              <h2>{message.sendByMe ? 'Tu' : "Pepe"}</h2>
+              <p>{message.content}</p>
+              <hr/>
+            </div>
+          }
+        )
+      }
 
-      <Contador/>
-      <Modal  
+
+
+
+
+      {/* <Contador/> */}
+     {/*  <Modal  
         openModal={openModal}
         onOpen={onOpen}
         onClose={onClose}
@@ -26,9 +91,9 @@ function App() {
           <h1>Aviso de falta de pago</h1>
           <p>Asegurate de estar al dia en tus cuentas</p>
         </div>
-      </Modal>
+      </Modal> */}
 
-      <button onClick={onOpen} onMouseLeave={onOpen}>Boton random</button>
+     {/*  <button onClick={onOpen} onMouseLeave={onOpen}>Boton random</button> */}
 
 
 
@@ -38,6 +103,10 @@ function App() {
           <p>Haz click <a href='https://estafa-legal.com'>Aqui</a> para reclamarlo</p>
         </div>
       </Modal> */}
+
+
+
+
     </div>
   )
 }
--- clase 21 - 22 - 23 - 24 - React/src/Components/Contador/Contador.jsx
+++ clase 21 - 22 - 23 - 24 - React/src/Components/Contador/Contador.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react"
 
+import useCounter from "../../hooks/useContador"
+
 /* 
 plantear el hook: useCounter(defaultCounterValue)
 La idea es que la funcionalidad de incremento y decremento del contador sea manejada en este hook useCounter asi de esta manera el Contador component queda mas limpio y solo se encarga de la renderizacion del contador.
@@ -8,34 +10,22 @@ El useCounter sera invocado desde Contador.jsx
 */
 function Contador (){
 
-    const [contador_estado_valor, setContadorEstado] = useState(0)
-
-    function incrementar (){
-
-        setContadorEstado(contador_estado_valor + 1)
-        console.log("[Incrementar] Ahora contador es: " + contador_estado_valor)
-    }
-
-    function decrementar(){
-
-        setContadorEstado(contador_estado_valor - 1)
-        console.log("[Decrementar] Ahora contador es: " + contador_estado_valor)
-    }
+    const {contador_estado, incrementar, decrementar} = useCounter(0)
 
     return (
         <div>
             <button onClick={decrementar}>-</button>
-            <span>{contador_estado_valor}</span>
+            <span>{contador_estado}</span>
             <button onClick={incrementar}>+</button>
             <br/>
             {
-                contador_estado_valor > 10 
+                contador_estado > 10 
                 && 
                 <span style={{color: 'red'}}>[ERROR]: El numero maximo a incrementar es 10</span>
             }
 
             {
-                contador_estado_valor < 0 
+                contador_estado < 0 
                 ? <span style={{color: 'red'}}>[ERROR]: El numero minimo es 0</span>
                 : <span style={{color: 'green'}}>El contador es valido</span>
             }
