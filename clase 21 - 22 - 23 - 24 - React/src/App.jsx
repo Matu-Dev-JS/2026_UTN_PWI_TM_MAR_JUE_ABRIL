f6cf367051165b313e4f002998c6b82e91a8a3d6
matu.dev <mati.a.gimenez2002@gmail.com>
2026-07-07 10:51:33 -0300

    Clase 25

--- clase 21 - 22 - 23 - 24 - React/src/App.jsx
+++ clase 21 - 22 - 23 - 24 - React/src/App.jsx
@@ -13,8 +13,18 @@ function App() {
     onClose
   } = useModal(true)
 
+  /* 
+  Crear un estado de mensajes con la lista de mensajes actual
+  Renderizar el estado de mensajes
 
-  const messages = [
+  Crear una funcion llamada crearMensaje(content, isMe)
+  Esta funcion debe agregar el mensaje al estado de mensajes
+
+  Crear un boton (al final de todos los mensajes/ o arriba) que sea <button>Crear mensaje de prueba</button> que al dar click nos active la funcion crearMensaje('Ves el partido hoy?', true) (o cualquier otro contenido)
+  */
+
+
+  const messages_server = [
     {
       id: 1,
       sendByMe: false,
@@ -37,19 +47,34 @@ function App() {
     }
   ]
 
-  /* 
-  Crear un estado de mensajes con la lista de mensajes actual
-  Renderizar el estado de mensajes
+  const [messages, setMessages] = useState(messages_server)
 
-  Crear una funcion llamada crearMensaje(content, isMe)
-  Esta funcion debe agregar el mensaje al estado de mensajes
+  
+  function crearMensaje (contenido, fuiYo){
 
-  Crear un boton (al final de todos los mensajes/ o arriba) que sea <button>Crear mensaje de prueba</button> que al dar click nos active la funcion crearMensaje('Ves el partido hoy?', true) (o cualquier otro contenido)
-  */
+    const nuevo_id = messages.length + 1
+    const nuevo_mensaje = {
+      content: contenido,
+      sendByMe: fuiYo,
+      id: nuevo_id
+    }
 
+    const messages_cloned = [...messages]
+    messages_cloned.push(nuevo_mensaje)
 
+    setMessages(messages_cloned)
+  }
 
-   
+  function handleCreateTestMessage (){
+    crearMensaje('Vas a ver el partido', true)
+  }
+
+  
+  //const numeros = [1, 2, 3]
+  
+  //const divs = numeros.map((numero) => {return <div>{numero}</div>})
+  //[<div>1</div>, <div>2</div>, <div>3</div>]
+  
   return (
     <div>
       <h1>Mensajes:</h1>
@@ -64,8 +89,7 @@ function App() {
           }
         )
       }
-
-      
+      <button onClick={handleCreateTestMessage}>Crear mensaje prueba</button>
 
      
 
